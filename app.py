e91ea174a23a585345e0e483bd4e2f4aa25fb8ad
Ramen16 <62827027+Ramen16@users.noreply.github.com>
2021-07-12 15:54:21 +0530

    ok

---
+++ app.py
@@ -0,0 +1,113 @@
+from flask import Flask,render_template,url_for,request
+import jsonify
+import requests
+import numpy as np
+import pandas as pd
+import nltk
+import seaborn as sns
+import matplotlib.pyplot as plt
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelBinarizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from wordcloud import WordCloud,STOPWORDS
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize,sent_tokenize
+from bs4 import BeautifulSoup
+import pickle
+
+import spacy
+import re,string,unicodedata
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.stem import LancasterStemmer,WordNetLemmatizer
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from textblob import TextBlob
+from textblob import Word
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+import nltk
+nltk.download('stopwords')
+
+## 1 positive 0 negative
+
+#Tokenization of text
+tokenizer=ToktokTokenizer()
+#Setting English stopwords
+stopword_list=nltk.corpus.stopwords.words('english')
+
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+#Removing the square brackets
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+
+#Removing the noisy text
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    return text
+#Define function for removing special characters
+def remove_special_characters(text, remove_digits=True):
+    pattern=r'[^a-zA-z0-9\s]'
+    text=re.sub(pattern,"",text)
+    return text
+
+#Stemming the text
+def simple_stemmer(text):
+    ps=nltk.porter.PorterStemmer()
+    text= ' '.join([ps.stem(word) for word in text.split()])
+    return text
+#Tokenization of text
+
+def remove_stopwords(text, is_lower_case=False):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
+
+#from Function import strip_html
+#from Function import remove_between_square_brackets
+#from Function import denoise_text
+#from Function import remove_special
+#from Function import stemmer
+#from Function import stop_word
+
+# load the model from disk
+with open('Countvector.pickle','rb') as f:
+    cv=pickle.load(f)
+    
+with open('logistic.pickle','rb') as f:
+    clf=pickle.load(f)
+    
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+        review = request.form["Review"]
+        sample=strip_html(review)
+        sample=remove_special_characters(sample)
+        sample=simple_stemmer(sample)
+        sample=remove_stopwords(sample)
+        sample=[sample]
+        vect = cv.transform(sample)
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
